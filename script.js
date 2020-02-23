84be63dff9b0e65e25ab4db4719e9a6edcdd622e
Alexey <40887676+alllekso@users.noreply.github.com>
2020-02-23 17:38:15 +0200

    JS: - replace "check_user_name" method on "login" method, switching between "check_name" and "check_pass" - add pass validation: min/max length, digits only CSS: - add min_height on "login_form" Other: refactored methods and class names

--- script.js
+++ script.js
@@ -2,46 +2,98 @@ let user_role;
 let user_pass;
 let notifications_field;
 let flamingo_pick_up = "Who's there?";
+let name_passed = false;
+
+function login() {
+    if (name_passed === false){
+        check_user_name();
+    }
+    else {
+        check_pass();
+    }
+
+}
 
 function show_login_login_form() {
     document.getElementById("login_form_is_hidden").hidden = false;
     document.getElementById("start_login_button").hidden = true;
-    setTimeout(ask_name,1000);
+    setTimeout(ask_name, 10);
 }
 
 function ask_name() {
-document.getElementById("hello_message").innerHTML = flamingo_pick_up;
+    document.getElementById("hello_message").innerHTML = flamingo_pick_up;
+    document.getElementsByClassName('login_group')[0].hidden = false;
+    document.getElementsByClassName('form_button')[0].hidden = false;
 }
 
-function check_is_admin() {
+function check_user_name() {
     user_role = document.getElementById("name").value;
-    notifications_field = document.getElementById("name_error_messages");
+    notifications_field = document.getElementById("name_notifications_field");
     notifications_field.hidden = false;
 
     if (user_role === 'Admin' || user_role === 'Lex') {
-        document.getElementById("pass_field").hidden = false;
+        document.getElementsByClassName('pass_group')[0].hidden = false;
         notifications_field.innerHTML = 'Name accepted';
         notifications_field.className = 'success_message';
-        wait(hide_message,"name_error_messages", 1500);
-    }
-    else if (user_role === 'Hacker' ){
+        wait(hide_message, "name_notifications_field", 2000);
+        name_passed = true;
+
+    } else if (user_role === 'Hacker') {
         notifications_field.innerHTML = 'You shall not pass!';
         notifications_field.className = 'warning_message';
-    }
-    else if (user_role === ""){
+    } else if (user_role === "") {
         notifications_field.innerHTML = 'At first enter your name';
         notifications_field.className = 'error_message';
-    }
-    else {
+    } else {
         notifications_field.innerHTML = 'User not found';
         notifications_field.className = 'error_message';
     }
+
 }
+
+function check_pass() {
+    user_pass = document.getElementById("pass").value;
+    notifications_field = document.getElementById("pass_notifications_field");
+    notifications_field.hidden = false;
+    if (user_pass === "") {
+        notifications_field.innerHTML = "Pass field is required!";
+        notifications_field.className = 'error_message'
+    }
+    if (validate(user_pass)) {
+
+    }
+}
+
+function validate(user_pass) {
+    notifications_field.innerHTML = "";
+    if (user_pass.length < 3) {
+        notifications_field.innerHTML = "Min pass length = 3";
+        notifications_field.className = 'error_message'
+    }
+
+    else if (user_pass.length > 11) {
+        notifications_field.innerHTML = "Max pass length = 10";
+        notifications_field.className = 'error_message'
+    }
+
+    else if (+(user_pass)){
+        notifications_field.innerHTML = "Pass can not contain digits only";
+        notifications_field.className = 'error_message'
+    }
+}
+
+
 function hide_message(element_id) {
 
-   document.getElementById(element_id).hidden = true;
+    document.getElementById(element_id).hidden = true;
 }
 
 function wait(func, element_id, time) {
-    setTimeout(function (){func(element_id);}, time);
+    setTimeout(function () {
+        func(element_id);
+    }, time);
 }
+
+
+
+
