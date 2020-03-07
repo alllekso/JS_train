db9dc27b6d6a01f47a03dd4c76a3b35fa1cfa493
Alexey <40887676+alllekso@users.noreply.github.com>
2020-03-07 22:06:13 +0200

    - refactoring for new architecture - sign up completed 90%

--- scripts/next_step.js
+++ scripts/next_step.js
@@ -1,15 +1,19 @@
 //не проходит валидация имени. фолс
 let action;
 let user_nickname;
-let user_pass;
+let password;
+let confirm_password = false;
 
 let pass_field;
 let notifications_field;
 let greetings_message;
 
-let name = false;
-let password = false;
-let confirm_password = false;
+let name_check = false;
+let pass_check = false;
+let confirm_pass_check = false;
+
+
+//
 
 function sign_up() {
     action = "sign up";
@@ -24,40 +28,64 @@ function login() {
 }
 
 function show_auth_form() {
-
-    fields_displaying_manager("class", "start_login_button", true);
-    fields_displaying_manager("class", "sign_up_button", true);
-    fields_displaying_manager("class", "sign_up_login_form", false);
+    set_hidden("class", "start_login_button", true);
+    set_hidden("class", "sign_up_button", true);
+    set_hidden("class", "sign_up_login_form", false);
     setTimeout(ask_nickname, 100);
 }
 
 function ask_nickname() {
-    document.querySelector(".chicken_message").innerHTML = greetings_message;
-    document.querySelector(".confirm_button").hidden = false;
+    set_hidden("class", "confirm_button", false);
+    send_notification("chicken_message", greetings_message);
+}
+
+function next_step() {
+    switch (action) {
+        case "login":
+            next_login_step();
+            break;
+        case  "sign up":
+            next_sign_up_step();
+            break;
+    }
+}
+
+function next_login_step() {
+    if (name_check === false) {
+        check_name();
+    } else {
+        check_pass();
+    }
+}
+
+function next_sign_up_step() {
+    if (name_check === false) {
+        check_name();
+    } else if (pass_check === false) {
+        check_pass();
+
+    } else if (confirm_password === false) {
+        check_confirm_pass();
+    } else if (confirm_pass_check === true){
+        save_user();
+    }
+}
+
+function check_name() {
+    user_nickname = get_element("id", "name").value;
+    name_validation();
 }
 
 function name_validation() {
-    user_nickname = document.getElementById("name").value;
-    notifications_field = document.getElementById("name_notifications_field");
-    notifications_field.hidden = false;
-}
-
-function check_user_nickname() {
-    user_nickname = document.getElementById("name").value;
-    notifications_field = document.getElementById("name_notifications_field");
-    pass_field = document.querySelector('.pass_group');
-    notifications_field.hidden = false;
+    set_hidden("id", "name_notifications_field", false);
 
     if (user_nickname === 'Lex') {
-        pass_field.hidden = false;
-        notifications_field.innerHTML = 'Name accepted';
-        notifications_field.className = 'success_message';
+        set_hidden("class", "pass_group", false);
+        send_notification("id", "name_notifications_field", "Name accepted");
+        set_class_name("id", "name_notifications_field", "success_message");
         wait(hide_message, "name_notifications_field", 2000);
-        name = true;
+        name_check = true;
 
-    } else if (user_nickname === 'Hacker') {
-        notifications_field.innerHTML = 'You shall not pass!';
-        notifications_field.className = 'warning_message';
     } else if (user_nickname === "") {
         notifications_field.innerHTML = 'At first enter your name';
         notifications_field.className = 'error_message';
@@ -75,10 +103,10 @@ function ask_confirm_pass() {
 }
 
 function check_pass() {
-    user_pass = document.getElementById("pass").value;
+    password = document.getElementById("pass").value;
     notifications_field = document.getElementById("pass_notifications_field");
     notifications_field.hidden = false;
-    pass_validation(user_pass);
+    pass_validation(password);
 }
 
 function check_confirm_pass() {
@@ -86,7 +114,13 @@ function check_confirm_pass() {
     notifications_field = document.getElementById("confirm_pass_notifications_field");
     notifications_field.hidden = false;
     pass_validation(confirm_password);
-    password = confirm_password ? save_user() : notifications_field.innerHTML = "Confirm password does not match the Password";
+    if (password === confirm_password){
+        confirm_pass_check = true;
+        next_sign_up_step();
+    }
+    else {
+        notifications_field.innerHTML = "Confirm password does not match the Password";
+    }
 }
 
 function pass_validation(user_pass) {
@@ -107,50 +141,15 @@ function pass_validation(user_pass) {
         notifications_field.innerHTML = "Pass can not contain digits only";
         notifications_field.className = 'error_message'
     } else {
-        password = true;
+        pass_check = true;
+        set_hidden("class", "confirm_pass_group", false);
         next_step();
     }
 }
 
 
-function next_step() {
-    switch (action) {
-        case "login":
-            next_login_step();
-            break;
-        case  "sign up":
-            next_sign_up_step();
-            break;
-    }
-}
-
-function next_login_step() {
-    if (name === false) {
-        check_user_nickname();
-    } else {
-        check_pass();
-    }
-}
-
-function next_sign_up_step() {
-    if (name === false) {
-        name === true ? pass_field.hidden = true : check_user_nickname();
-    } else if (password === false) {
-        // password === true ? check_confirm_pass() : check_pass();
-
-        if (password === true) {
-            ask_confirm_pass();
-        } else check_pass();
-
-    } else if (confirm_password === false) {
-        check_confirm_pass();
-    }
-}
-
 function hide_message(element_id) {
-
-    // document.getElementById(element_id).hidden = true;
-    fields_displaying_manager("id", element_id, true);
+    set_hidden("id", element_id, true);
 }
 
 function wait(func, element_id, time) {
@@ -162,7 +161,7 @@ function wait(func, element_id, time) {
 function create_user() {
     function new_user() {
         this.nickname = user_nickname;
-        this.password = user_pass;
+        this.password = password;
     }
 
     return new_user();
@@ -172,17 +171,35 @@ function save_user() {
     alert("saved");
 }
 
-function get_selector_value(type, selector) {
-    let selector_value;
+function get_element(type, selector) {
+    let element;
     switch (type) {
-        case "class": selector_value = document.querySelector("." + selector);
-        break;
-        case "id": selector_value = document.getElementById(selector);
-        break;
+        case "class":
+            element = document.querySelector("." + selector);
+            break;
+        case "id":
+            element = document.getElementById(selector);
+            break;
+        case  "var":
+            element = selector;
+            break;
     }
-    return selector_value;
+    return element;
 }
 
-function fields_displaying_manager(type, selector, hidden) {
-    get_selector_value(type, selector).hidden = hidden;
+function get_value(type, selector) {
+    get_element(type, selector).value;
+}
+
+function set_hidden(type, selector, hidden) {
+    get_element(type, selector).hidden = hidden;
+}
+
+function send_notification(type, selector, message) {
+    get_element(type, selector, message).innerHTML = message;
+}
+
+function set_class_name(type, selector, class_name) {
+    get_element(type, selector, class_name).className = class_name;
+
 }
