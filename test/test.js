4d9c14b8d0cc7fb5b2002d3c499e2063142b3c52
KateKrav <laylovalova@gmail.com>
2020-03-11 14:00:24 +0200

    -start js

--- test/test.js
+++ test/test.js
@@ -15,3 +15,26 @@ function subtraction() {
     result = first_digit - second_digit;
     document.getElementById("result").innerHTML = result;
 }
+
+
+
+function keys1() {
+    from_get = document.getElementById("button-namb1").innerHTML;
+    alert(from_get);
+}
+function keys2() {
+    from_get = document.getElementById("button-namb2").innerHTML;
+    alert(from_get);
+}
+function keys3() {
+    from_get = document.getElementById("button-namb3").innerHTML;
+    alert(from_get);
+}
+function keys4() {
+    from_get = document.getElementById("button-namb4").innerHTML;
+    alert(from_get);
+}
+function keys5() {
+    from_get = document.getElementById("button-namb5").innerHTML;
+    alert(from_get);
+}
