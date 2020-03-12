619a4488d82d93eda4045ea12c82c4216c464d7e
KateKrav <laylovalova@gmail.com>
2020-03-12 21:31:32 +0200

    -div"history" CSS style, -JS (-). (*). (/).

--- test/test.js
+++ test/test.js
@@ -21,80 +21,75 @@ function connect_display() {
 
 //  калькулятор
 
-function sum() {
-    action = "sum";
-    result = input;
-    save_history(input, " + ");
 
-    input = "";
-
-}
-
-function save_history(log_data_1, log_data_2) {
-    history_log = history_log + log_data_1 + log_data_2;
-    display_history.innerHTML = history_log;
-
-}
 
 function key1() {
     input = Number(input + "1");
     display_element.innerHTML = input;
-
-
 }
-function key2() {
-    input = input + "2";
-    display_element.innerHTML = input;
 
+function key2() {
+    input = Number(input + "2");
+    display_element.innerHTML = input;
 }
 function key3() {
-    input =  input + "3";
-    display_element.innerHTML =  input;
+    input = Number(input + "3");
+    display_element.innerHTML = input;
 }
 function key4() {
-    display_element.value =  input + "4";
-    display_element.innerHTML =  input;
+    input = Number(input + "4");
+    display_element.innerHTML = input;
 }
 function key5() {
-    input =  input + "5";
+    input = Number(input + "5");
     display_element.innerHTML = input;
 }
 function key6() {
-    input =  input + "6";
-    display_element.innerHTML =  input;
+    input = Number(input + "6");
+    display_element.innerHTML = input;
 }
 function key7() {
-    input =  input + "7";
-    display_element.innerHTML =  input;
+    input = Number(input + "7");
+    display_element.innerHTML = input;
 }
 function key8() {
-    input =  input + "8";
-    display_element.innerHTML =  input;
+    input = Number(input + "8");
+    display_element.innerHTML = input;
 }
 function key9() {
-    input =  input + "9";
-    display_element.innerHTML =  input;
+    input = Number(input + "9");
+    display_element.innerHTML = input;
 }
 function key0() {
-    input =  input + "0";
-    display_element.innerHTML =  input;
+    input = Number(input + "0");
+    display_element.innerHTML = input;
 }
 
 
 // Арефметические знаки
 
+function sum() {
+    action = "sum";
+    result = input;
+    save_history(input, " + ");
+    input = "";
+}
 
+function save_history(log_data_1, log_data_2) {
+    history_log = history_log + log_data_1 + log_data_2;
+    display_history.innerHTML = history_log;
+}
 
 function ask_next_digit() {
     input = ""
 }
 
-
-
 function clean() {
     result = input = "";
     display_element.innerHTML = result;
 }
+
+
 
 function minus() {
     action = "minus";
@@ -129,19 +124,23 @@ function calculate_result() {
         case "minus":
             result = result - input;
             display_element.innerHTML = result;
+            save_history(input, " = ");
+            save_history(result, "");
             break;
 
         case "multiply":
             result = result * input;
             display_element.innerHTML = result;
+            save_history(input, " = ");
+            save_history(result, "");
             break;
 
         case "division":
             result = result / input;
             display_element.innerHTML = result;
+            save_history(input, " = ");
+            save_history(result, "");
             break;
-
-
     }
 
 
