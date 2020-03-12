786ab94c4208015b243d9a93be7e1b7b4bd42035
KateKrav <laylovalova@gmail.com>
2020-03-12 14:33:21 +0200

    -fix js calculator

--- test/test.js
+++ test/test.js
@@ -27,11 +27,45 @@ function key1() {
 display.value = display.value + "1";
 display.innerHTML = display.value;
 }
-
 function key2() {
 display.value = display.value + "2";
 display.innerHTML = display.value;
 }
+function key3() {
+    display.value = display.value + "3";
+    display.innerHTML = display.value;
+}
+function key4() {
+    display.value = display.value + "4";
+    display.innerHTML = display.value;
+}
+function key5() {
+    display.value = display.value + "5";
+    display.innerHTML = display.value;
+}
+function key6() {
+    display.value = display.value + "6";
+    display.innerHTML = display.value;
+}
+function key7() {
+    display.value = display.value + "7";
+    display.innerHTML = display.value;
+}
+function key8() {
+    display.value = display.value + "8";
+    display.innerHTML = display.value;
+}
+function key9() {
+    display.value = display.value + "9";
+    display.innerHTML = display.value;
+}
+function key0() {
+    display.value = display.value + "0";
+    display.innerHTML = display.value;
+}
+
+
+// Арефметические знаки
 
 function sum2() {
     result = +display.value;
@@ -42,6 +76,27 @@ function sum2() {
 function ask_next_digit() {
     display.value = ""
 }
+
+
+
+function clean() {
+    result = display.value= "";
+    display.innerHTML = result;
+}
+
+function minus() {
+    result = -display.value;
+    ask_next_digit(); //когда одно значение больше другого и значение больше идет первым пишет (-1)
+}
+
+function multiply() {
+    result =   display * result;
+    ask_next_digit();
+
+}
+
+
+
 
 function calculate_result() {
     result = +result + +display.value;
