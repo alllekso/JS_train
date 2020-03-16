4c7bb7b7bc840862e58246d29127d2ecb2900fc4
KateKrav <laylovalova@gmail.com>
2020-03-17 00:35:33 +0200

    -js calculator start progress

---
+++ test/test.js
@@ -0,0 +1,48 @@
+let number1;
+let number2;
+let result;
+let key;
+
+
+function sum() {
+    number1 = document.getElementById("first").value;
+    number2 = document.getElementById("second").value;
+    result = +number1 + +number2 ;
+    show_result();
+}
+
+
+function show_result() {
+    document.getElementById("display").value = result;
+}
+
+function subtraction() {
+    number1 = document.getElementById("first").value;
+    number2 = document.getElementById("second").value;
+    result = +number1 - +number2 ;
+    show_result();
+}
+
+function division() {
+    number1 = document.getElementById("first").value;
+    number2 = document.getElementById("second").value;
+    result = +number1 / +number2 ;
+    show_result();
+}
+
+function multiplication() {
+    number1 = document.getElementById("first").value;
+    number2 = document.getElementById("second").value;
+    result = +number1 * +number2 ;
+    show_result();
+}
+
+function key2() {
+    key = +2;
+    document.getElementById("first").value = key;
+}
+
+function key3() {
+    key = +3;
+    document.getElementById("second").value = key;
+}
