8055318d6847e5324e16374c645692f241ceb0cf
KateKrav <laylovalova@gmail.com>
2020-03-24 21:18:48 +0200

    -js

--- test/Full Calculator/F-Calcul.js
+++ test/Full Calculator/F-Calcul.js
@@ -1,88 +1,44 @@
 "use strict";
 
 
-let button_1;
-let button_2;
-let button_3;
-let button_4;
-let button_5;
-let button_6;
-let button_7;
-let button_8;
-let button_9;
-let button_0;
-
-let button_plus;
-let button_minus;
-let button_multy;
-let button_delenie;
-let button_cleen;
-let button_delete;
-
 let display;
+let result = 0;
+let current_value = "";
+let history;
 
 
 
-
-function get_page_elements() {
-    button_1 = document.getElementById("key1");
-    button_2 = document.getElementById("key2");
-    button_3 = document.getElementById("key3");
-    button_4 = document.getElementById("key4");
-    button_5 = document.getElementById("key5");
-    button_6 = document.getElementById("key6");
-    button_7 = document.getElementById("key7");
-    button_8 = document.getElementById("key8");
-    button_9 = document.getElementById("key9");
-    button_0 = document.getElementById("key0");
-
-    button_plus = document.getElementById("key0");
-    button_minus = document.getElementById("key0");
-    button_multy = document.getElementById("key0");
-    button_delenie = document.getElementById("key0");
-    button_cleen = document.getElementById("key0");
-    button_delete = document.getElementById("key0");
-    display = document.getElementById("display");
-    display.value = "";
-}
-
-
-
-function but1(){
+function key1(){
     document.getElementById("display").innerHTML = "1";
+    current_value = current_value + "1";
 }
 
 
-
-
-
-
-
-function elements_key() {
-    button_1.value = 1;
-
-
-
-
-    button_2.value = 2;
-    button_3.value = 3;
-    button_4.value = 4;
-    button_5.value = 5;
-    button_6.value = 6;
-    button_7.value = 7;
-    button_8.value = 8;
-    button_9.value = 9;
-    button_0.value = 0;
+function key2() {
+    document.getElementById("display").innerHTML = "2";
+    current_value = current_value + "2";
 }
 
-function display_show() {
-    document.getElementById("display").innerHTML = display;
+
+function plus() {
+    result = result + +current_value;
+    current_value = "";
 
 }
 
-function element() {
-    display = button_1;
-    display_show();
+function ravno() {
+    result = result + +current_value;
+    // display = (current_value + "+" + current_value + "=" + result);
+    // history = history + display;
+    showdisplay();
+    // display_result();
+}
 
+function showdisplay() {
+    document.getElementById("display").innerHTML = result;
 
 }
+
+// function display_result() {
+//     document.getElementById("display").innerHTML = history;
+// }
